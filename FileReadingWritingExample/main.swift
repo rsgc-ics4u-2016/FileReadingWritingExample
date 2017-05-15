eab3a5ac2fff6a80426b976b32debd459914e321
Russell Gordon <rgordon@rsgc.on.ca>
2017-05-15 12:33:55 -0400

    Initial Commit

---
+++ FileReadingWritingExample/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FileReadingWritingExample
+//
+//  Created by Russell Gordon on 5/15/17.
+//  Copyright © 2017 Russell Gordon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
